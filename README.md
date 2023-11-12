48c28167edc45f8b4eeda3aecc4ff147a2be7eea
ian <iandkaneshiro@gmail.com>
2023-11-11 20:38:09 -0800

    reademe update

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# nfl-challenge
+# Nfl-challenge
+
+Learning Redux ToolKit, and React Router Dom v6+
+As well as trying out vite
--- store/counter/counterSlice.js
+++ store/counter/counterSlice.js
@@ -7,7 +7,11 @@ const initialState = {
 const counterSlice = createSlice({
   name: "counter",
   initialState,
-  reducers: {},
+  reducers: {
+    increment: (state) => {
+      state.value += 1;
+    },
+  },
 });
 
 export default counterSlice.reducer;
